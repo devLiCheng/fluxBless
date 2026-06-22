7711ea3db0099e7e34a5fe3cd5d5912d42311fe3
licheng <819812520@qq.com>
2026-06-22 13:48:15 +0800

    feat: configure Sentry monitoring for frontend, backend, and admin

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import { withSentryConfig } from "@sentry/nextjs";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  // For details, see: https://github.com/getsentry/sentry-javascript/blob/master/packages/nextjs/README.md
+  silent: true,
+  org: "cheng-1d",
+  project: "javascript-nextjs",
+});
